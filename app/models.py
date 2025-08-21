93a76707e76053be42e778aea6f2867e0f8a0c3f
Julius Kanani <juliuskanani.softwarengineer@gmail.com>
2025-08-21 11:48:01 +0300

    feat: add Task model with to_dict method for SQLAlchemy ORM

---
+++ app/models.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+from app import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Boolean, default=False)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'completed': self.completed
+        }
