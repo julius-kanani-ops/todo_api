5b9ab608eea4fd13b7fe97cddeedd4fa7f5e4e76
Julius Kanani <juliuskanani.softwarengineer@gmail.com>
2025-08-21 12:24:34 +0300

    fix: correct typo in SQLALCHEMY_DATABASE_URI environment variable name

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABSE_URI = os.environ.get('DATABASE_URL') or\
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or\
     'sqlite:///' + os.path.join(base_dir, 'tasks.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
