aabf7ac36bdf2f4853ba1a9de15eaed81948400b
Julius Kanani <juliuskanani.softwarengineer@gmail.com>
2025-08-21 14:02:55 +0300

    fix(app/models.py): changed the correct datatype for description from Boolean to String, and added the completed attribute to the Task model.

--- app/models.py
+++ app/models.py
@@ -7,7 +7,8 @@ from app import db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Boolean, default=False)
+    description = db.Column(db.String(200), nullable=True)
+    completed = db.Column(db.Boolean, default=False)
 
 
     def to_dict(self):
