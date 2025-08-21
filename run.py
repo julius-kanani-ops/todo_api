44bd1ed2c68fd36d4d6e207adc2dc000f9bf9d7b
Julius Kanani <juliuskanani.softwarengineer@gmail.com>
2025-08-21 12:12:23 +0300

    feat: add application entry point and shell context for Flask CLI

---
+++ run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+from app import create_app, db
+from app.models import Task
+
+
+app = create_app()
+
+
+# This is a useful Flask feature that lets you create a shell context
+# to work with your app and database directly from the terminal.
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Task': Task}
+
+if __name__ == "__main__":
+    app.run(debug=True)
